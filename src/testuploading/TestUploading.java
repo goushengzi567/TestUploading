22bedba1d4928137cacb6cb9d3888c928ddf0cd6
apple1 <apple1@149.125.82.183>
2016-09-12 03:32:31 -0400

    new commit

---
+++ src/testuploading/TestUploading.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testuploading;
+
+/**
+ *
+ * @author apple1
+ */
+public class TestUploading {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("This is a test for uploading java projects to gitHub");
+    }
+    
+}
